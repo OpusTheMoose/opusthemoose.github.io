58f35e871fdb53679b2262cea7ead0d4b1dc968a
Opus <opusthemoose@gmail.com>
2026-08-24 11:28:01 -0600

    updated config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: '/personal-website-v2/',
   build: {
     rollupOptions: {
       input: {
