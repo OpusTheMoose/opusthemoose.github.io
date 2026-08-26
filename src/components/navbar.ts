6fd75627846fb4a0c886a754d8d5ab9b616799de
Opus <opusthemoose@gmail.com>
2026-08-26 11:53:54 -0600

    fixed a couple issues

--- src/components/navbar.ts
+++ src/components/navbar.ts
@@ -2,7 +2,7 @@ class Navbar extends HTMLElement {
     connectedCallback() {
       this.innerHTML = `<nav class="navbar">
       <div class="navbar-left">
-        <button class="nav-button" onclick="location.href='${import.meta.env.BASE_URL}/index.html'">HOME</button>
+        <button class="nav-button" onclick="location.href='${import.meta.env.BASE_URL}index.html'">HOME</button>
       </div>
       <div class="navbar-center">
         <button class ="nav-button" onclick="location.href='${import.meta.env.BASE_URL}src/pages/portfolio.html'">PORTFOLIO</button>
